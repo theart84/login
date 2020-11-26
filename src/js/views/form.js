27b8a15bb02070712ec9d481c61e8f021f9b09ac
theart84 <artshadow74@gmail.com>
2020-11-26 18:49:44 +0500

    Add form validation

---
+++ src/js/views/form.js
@@ -0,0 +1,35 @@
+/**
+ * Function input error template
+ * @param msg
+ * @return {string}
+ */
+function inputErrorTemplate(msg) {
+  return `
+    <div class="invalid-feedback">${msg}</div>
+  `;
+}
+/**
+ * Function show error. Add input error.
+ * @param {HTMLInputElement}
+ */
+export function showInputError(el) {
+  const parent = el.parentElement;
+  const msg = el.dataset.invalidMessage || 'Invalid input';
+  const template = inputErrorTemplate(msg);
+  el.classList.add('is-invalid');
+  parent.insertAdjacentHTML('beforeend', template);
+}
+
+/**
+ * Function remove error. Remove input error.
+ * @param el
+ */
+export function removeInputError(el) {
+  const parent = el.parentElement;
+  const err = parent.querySelector('.invalid-feedback');
+  if (!err) return;
+
+  el.classList.remove('is-invalid');
+  parent.removeChild(err);
+}
+
